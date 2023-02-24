d5eaa952158efaf1e91efd3d7f675d3db0afb546
Grant Simon <grant.simon-2@selu.edu>
2023-02-23 18:05:34 -0600

    Added comments for implementing data
    get documents from david for implementation

--- MPapp.py
+++ MPapp.py
@@ -3,8 +3,40 @@ import plotly.express as px
 import pandas as pd
 from dash.dependencies import Input, Output
 
-app = Dash(__name__)
+#######################################
+"""
+    NOTES 
+filereader.py should be in the same folder as this file.
+As should DataVisualization.py
 
+you will need to pip install mne, plotly, dash_bootstrap_components, pandas, IPython.display
+
+import DataVisualization
+
+filereader = FileReader.FileReader()
+filereader.setRawData()
+filereader.setDataFrame()
+
+#if testing semi-dry-demo-signals.bdf uncomment the below
+###
+#filereader.raw_data.rename_channels({'M1':'TP9'})
+#filereader.raw_data.rename_channels({'M2':'TP9'})
+#filereader.raw_data.drop_channels('Status')
+#filereader.raw_data.set_montage(mne.channels.make_standard_montage('easycap-M1'))
+###
+
+raw_ = DataVisualization.DataVisualization(filereader.raw_data, filereader.raw_df)
+
+###
+
+
+
+"""
+
+#######################################
+app = Dash(__name__)
+# UNCOMMENT BELOW LINE WHEN YOU RESOLVE THE NOTES ABOVE
+# container_2dHeatmap = raw.get_2dHeatmap_child(app)
 app.layout = html.Div(
     className='bar',
     children=[
@@ -28,6 +60,7 @@ app.layout = html.Div(
             parent_className='custom-tabs',
             className='custom-tabs-container',
             children=[
+
                 dcc.Tab(
                     label='Tab One',
                     value='tab-1',
@@ -65,7 +98,10 @@ def render_content(tab):
         ])
     elif tab == 'tab-2':
         return html.Div([
-            html.H3('Tab content 2')
+            html.H3('Tab content 2'),
+            html.H3('Hopefully this returns a 2d heatmap'),
+            # UNCOMMENT BELOW LINE WHEN YOU UNCOMMENT THE ONE BEFORE app.layout = ...
+            # html.Div(container_2dHeatmap, style = {'width':'49%','display':'inline-block'})
         ])
     elif tab == 'tab-3':
         return html.Div([
