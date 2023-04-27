47223c3c60806f8c015eb3e12d60a0c722cbadd1
Grant Simon <grant.simon-2@selu.edu>
2023-04-27 13:26:59 -0500

    Added a Logo

--- pages/home.py
+++ pages/home.py
@@ -3,19 +3,22 @@ from dash import html, dcc
 
 dash.register_page(__name__, path='/')
 
-layout =html.Div(
-         className='bar',
-         children=[
-
-             # Header
-             html.H1(
-                 className='header',
-                 children='MindPrint',
-             ),
-
-             # SubHeader
-             html.Div(
-                 className='subHeader',
-                 children='''Exploring the uniqueness of individuals' brains.''',
-             ),
-])
+layout = html.Div(
+    className='container',
+    children=[
+        html.Div(
+            className='header',
+            children=[
+                html.Img(
+                    src='/assets/logo.png',
+                    className='logo'
+                ),
+                html.H1(children='MindPrint'),
+            ]
+        ),
+        html.Div(
+            className='subheader',
+            children='Exploring the uniqueness of individuals\' brains.'
+        )
+    ]
+)
