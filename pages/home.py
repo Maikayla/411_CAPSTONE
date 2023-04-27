853640c3e7a335f7ef2410fc9ab9e4f0148569e9
Grant Simon <grant.simon-2@selu.edu>
2023-04-27 16:01:34 -0500

    Added Description of Project to Home Page

--- pages/home.py
+++ pages/home.py
@@ -18,6 +18,18 @@ layout = html.Div(
         html.Div(
             className='subheader',
             children='Exploring the uniqueness of individuals\' brains.'
+        ),
+        html.Div(
+            className='mindprint-text',
+            children=[
+                html.P('MindPrint is a research project at SELU, headed by Dr. Ömer Soysal that explores uniqueness of individuals’ brain signals and aims to:'),
+                html.Ul([
+                    html.Li(
+                        'Develop a system to explore specific characteristics of individuals’ brain signals'),
+                    html.Li(
+                        'Build and implement a curriculum to train the workforce for computational aspects of brain signals')
+                ])
+            ]
         )
     ]
 )
