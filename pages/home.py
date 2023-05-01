8fc82a2d284ebb8ccf5dfcb68dd3257fcfa47a32
Grant Simon <grant.simon-2@selu.edu>
2023-05-01 15:42:53 -0500

    Remove lists on home page

--- pages/home.py
+++ pages/home.py
@@ -1,10 +1,10 @@
 import dash
 from dash import html, dcc
 
-dash.register_page(__name__, path='/')
+dash.register_page(name, path='/')
 
 layout = html.Div(
-    className='container',
+    style={'text-align': 'center'},
     children=[
         html.Div(
             className='header',
@@ -17,18 +17,18 @@ layout = html.Div(
         ),
         html.Div(
             className='subheader',
-            children='Exploring the uniqueness of individuals\' brains.'
+            style={'font-weight': 'bold', 'font-size': '20px'},
+            children="Exploring the uniqueness of individuals' brains"
         ),
         html.Div(
             className='mindprint-text',
+            style={'font-size': '16px'},
             children=[
                 html.P('MindPrint is a research project at SELU, headed by Dr. Ömer Soysal that explores uniqueness of individuals’ brain signals and aims to:'),
-                html.Ul([
-                    html.Li(
-                        'Develop a system to explore specific characteristics of individuals’ brain signals'),
-                    html.Li(
-                        'Build and implement a curriculum to train the workforce for computational aspects of brain signals')
-                ])
+                html.P(
+                    '- Develop a system to explore specific characteristics of individuals’ brain signals'),
+                html.P(
+                    '- Build and implement a curriculum to train the workforce for computational aspects of brain signals')
             ]
         )
     ]
